83621e6d896c54c741841f029692ffff9b8a24a0
rafael.dornelles <rafafd@hotmail.com>
2021-09-04 11:48:07 -0300

    Initial Commit

---
+++ NotasApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotasApp
+//
+//  Created by IOS SENAC on 9/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotasApp/NotasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotasAppApp.swift
+//  NotasApp
+//
+//  Created by IOS SENAC on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct NotasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
